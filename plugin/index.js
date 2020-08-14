3873733eb76f8da3503a52ec661c5f83124e2f87
Saurabh Daware <saurabhdaware99@gmail.com>
2020-08-14 19:30:20 +0530

    add required values comment

--- plugin/index.js
+++ plugin/index.js
@@ -9,11 +9,12 @@ function beforeBuild(programInfo, { createContent }) {
   console.log('I can also add new blog, look!');
 
   // For Source Plugins
+
+  // slug, title, content, createdAt, modifiedAt are required values
   const sourceNode = {
     slug: 'blog-from-plugin',
     title: 'Hi, This blog comes from plugin',
-    content: `# Hello\nCheck out your './plugin/index.js' file.
-    `,
+    content: `# Hello\nCheck out your './plugin/index.js' file.`,
     createdAt: new Date('13 May 2019'),
     modifiedAt: new Date('13 May 2019'),
     foo: 'bar' // can be accessed with meta.foo
