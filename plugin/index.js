fdba1f2f87a8c044a6e7dcf61bbd4f1c4633900f
Saurabh Daware <saurabhdaware99@gmail.com>
2020-08-14 19:12:11 +0530

    init plugin code

---
+++ plugin/index.js
@@ -0,0 +1,37 @@
+
+/**
+ * Executed before build. Access to createContent function makes it ideal for source plugins.
+ * @param {import('abell').ProgramInfo} programInfo
+ * @param {object} options
+ * @param {Function} options.createContent
+ */
+function beforeBuild(programInfo, { createContent }) {
+  console.log('I am executed before the build starts.');
+  console.log('I can also add new blog, look!');
+
+  // For Source Plugins
+  const sourceNode = {
+    slug: 'blog-from-plugin',
+    title: 'Hi, This blog comes from plugin',
+    content: `# Hello
+
+    Check out your './plugin/index.js' file.
+    `,
+    createdAt: new Date('13 May 2019'),
+    modifiedAt: new Date('13 May 2019'),
+    foo: 'bar'
+  };
+
+  createContent(sourceNode);
+}
+
+/**
+ * Executed after build. Ideal for generating rss, sitemap, etc.
+ * @param {import('abell').ProgramInfo} programInfo
+ */
+function afterBuild(programInfo) {
+  console.log('I am executed after the build');
+}
+
+
+module.exports = { beforeBuild, afterBuild }
